2fb8162884578c39d8bdd18225d28786b4336c60
YahiaJouini <jouiniyahya117@gmail.com>
2023-09-23 13:23:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const city=document.querySelector('.text-input');
+const cityDisplay=document.querySelector('.location');
+const temp=document.querySelector('h1');
+const status=document.querySelector('.status');
+const search=document.querySelector('button');
+
+
+const icons =
+    {
+    cloudy :'cloudy.png',
+    sunny : "sun.png",
+    rainy: 'rain.png'
+    };
+
+
+const key='aa6a2250b1d83fd114cfa3f99ee54a6b';
